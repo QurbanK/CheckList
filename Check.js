ccfc3574665f3e57c1d001b4fc863958be455367
QurbanK <107764347+QurbanK@users.noreply.github.com>
2023-04-09 15:05:43 +0530

    Create Check.js

---
+++ Check.js
@@ -0,0 +1,41 @@
+const handleCheckboxClick = (e, data) => {
+    const checked = e.target?.checked;
+    let checked_list = [...selectedCheckList];
+    if (checked) {
+      checked_list.push(data?.payment_mapping_reference_number);
+    } else {
+      checked_list = checked_list.filter(
+        (item) => item !== data?.payment_mapping_reference_number
+      );
+    }
+    setSelectedCheckList(checked_list);
+    console.log({ checked_list });
+  };
+  
+  //Then send the data payload//
+  
+    const submitDuesHandler = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const payload = {
+      payment_mapping_reference_number: selectedCheckList,
+    };
+    duesReverse(payload)
+      .then(
+        (res) => {
+          if (res && res.success) {
+            setLoading(false);
+
+            setSelectedCheckList([]);
+            toastSuccess(res.message);
+          }
+        },
+        (err) => {
+          setLoading(false);
+        }
+      )
+      .catch((err) => {
+        setLoading(false);
+        toastError(err.message);
+      });
+  };
