e02e3f32c8ecca6f54ad888af60f6017b204e903
QurbanK <107764347+QurbanK@users.noreply.github.com>
2023-10-23 00:12:17 +0530

    Name of student.js

--- Check.js
+++ Check.js
@@ -1,3 +1,31 @@
+//student of nios
+1.ronak
+2.govinda
+3.gaytri
+4.zaid
+5.aasif
+6.aman kumar
+7.ahasan
+8.amara
+9.najrana
+10.arbaaz
+11.heena
+12.shahrukh khan
+13.dev
+14.javed
+15.aadi
+16.rihan
+17.anas
+18.muskan
+19.sofiya
+20.aditya
+21.noni
+22.noor
+
+
+
+
+
 const handleCheckboxClick = (e, data) => {
     const checked = e.target?.checked;
     let checked_list = [...selectedCheckList];
